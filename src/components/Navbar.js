d612e9add0ffe636e9b986fa5d87c49e733f5a25
jah <hilliardjoel05@gmail.com>
2023-08-25 23:25:09 -0600

    Mobile friendlty navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,15 +18,19 @@ function Navbar(){
                         </Link>
 
                         {/* Hamburger Menu Icon for mobile */}
-                        <button onClick={() => setMenu(!menu)} className="md:hidden px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
-                            🍔
+                        <button onClick={() => setMenu(!menu)} className="md:hidden px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white transform active:scale-95">
+                            <svg viewBox="0 0 100 80" width="20" height="20" >
+                                <rect width="100" height="20" fill="#ffffff"></rect>
+                                <rect y="30" width="100" height="20" fill="#ffffff"></rect>
+                                <rect y="60" width="100" height="20" fill="#ffffff"></rect>
+                            </svg>
                         </button>
                     </div>
 
                     {/* Link items */}
                     <div className={`${menu ? 'flex flex-col' : 'hidden'} w-full md:w-auto md:flex md:items-center md:space-x-2 mt-2 md:mt-0`}>
-                        <Link onClick={()=>setMenu(!menu)} to="/matchups" className="block hover:bg-green-700 px-2 py-1 rounded transform active:scale-95 font-bold whitespace-nowrap">Historical Matchups</Link>
-                        <Link onClick={()=>setMenu(!menu)} to="/leaderboard" className="block md:mt-0 hover:bg-green-700 px-2 py-1 rounded transform active:scale-95 font-bold">Leaderboard</Link>
+                        <Link onClick={()=>setMenu(false)} to="/matchups" className="block hover:bg-green-700 px-2 py-1 rounded transform active:scale-95 font-bold whitespace-nowrap">Historical Matchups</Link>
+                        <Link onClick={()=>setMenu(false)} to="/leaderboard" className="block md:mt-0 hover:bg-green-700 px-2 py-1 rounded transform active:scale-95 font-bold">Leaderboard</Link>
                     </div>
                 </div>
             </div>
