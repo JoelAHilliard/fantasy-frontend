89929aa335122b009e51487abc36e588663f75dc
Jacob Williams <jacobmiwilliams@gmail.com>
2023-08-26 17:29:55 -0500

    Hoff

--- src/components/Hoff.js
+++ src/components/Hoff.js
@@ -8,11 +8,11 @@ return (
 <h1>Hoff's Matchups of the Week</h1>
 <ol class="ranking-list">
     <li>Marsha vs. Becca</li>
-    <li>China</li>
-    <li>India</li>
-    <li>Brazil</li>
-    <li>Russia</li>
-    <li>Russia</li>
+    <li>China vs. Phil</li>
+    <li>Phaucette vs. Jacob</li>
+    <li>Clog vs. Calvin</li>
+    <li>Mad dawg vs. Wes</li>
+    <li>Joel vs. Jayrod</li>
 </ol>
 </div>
 
