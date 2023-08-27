2f5c17ef433a13e2acd5620d027e18eba8e24d00
Jacob Williams <jacobmiwilliams@gmail.com>
2023-08-27 14:06:49 -0500

    chili clog

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+import "./Quote.css"
+
+function Quote() {
+    return(
+
+        <div className="px-4">
+
+        <h2>Todays Quote of the Day: Who clogged the faucette?</h2>
+
+        </div>
+
+)
+}
+
+export default Quote;
