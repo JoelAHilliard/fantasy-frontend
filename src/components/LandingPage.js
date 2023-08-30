6e5b9d81b64ed21cdda21abfe9942df0335f3936
Jacob Williams <jacobmiwilliams@gmail.com>
2023-08-29 20:59:06 -0500

    Added polling and styling for it

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,17 @@
 import Hoff from "./Hoff";
+import Quote from "./Quote"
+import Poll from './Poll'
 
 function LandingPage(){
     return(
+
+        <div>
+
+        <Quote></Quote>
         <Hoff></Hoff>
+        <Poll></Poll>
+        
+        </div>
     )
 }
 
