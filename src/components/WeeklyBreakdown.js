f9afa138f624b49843809685100addef55008d72
jah <hilliardjoel05@gmail.com>
2023-09-13 13:31:59 -0600

    Fix crown location, add ovewrflow protection

--- src/components/WeeklyBreakdown.js
+++ src/components/WeeklyBreakdown.js
@@ -41,7 +41,7 @@ function WeeklyBreakdown(){
                 <div className="flex flex-row overflow-x-scroll gap-2">
                     {data ? data['matchups'].map((matchup)=>{
                         return(
-                            <div key={matchup.matchupNum} className="grid grid-cols-2 gap-5 text-center rounded-lg border-b-4 bg-gray-200 border-green-600 shadow-lg px-2 text-xs text-base sm:text-sm md:text-sm lg:text-lg whitespace-nowrap min-w-fit py-4">
+                            <div key={matchup.matchupNum} className="grid grid-cols-2 gap-5 text-center rounded-lg border-b-4 bg-gray-200 border-green-600 shadow-lg px-2 text-xs text-base sm:text-sm md:text-sm lg:text-lg whitespace-nowrap min-w-fit py-4 truncate">
                                 {/* Team Headers & Scores */}
                                 <div className="col-span-1 text-right" id="home_team">
                                     
@@ -62,7 +62,7 @@ function WeeklyBreakdown(){
                                     {
                                         
                                         matchup.home_score < matchup.away_score ? 
-                                            <div className='font-semibold'>{matchup.away_team} - 👑</div> 
+                                            <div className='font-semibold'>👑 - {matchup.away_team}</div> 
                                         :   <div className='font-semibold'>{matchup.away_team}</div>
                                      
 
