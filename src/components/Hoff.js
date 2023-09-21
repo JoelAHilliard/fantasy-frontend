edac873f540f04c64a2719cf4509e5a64f9372b2
jah <hilliardjoel05@gmail.com>
2023-09-20 22:07:26 -0600

    Clean up ladning page

--- src/components/Hoff.js
+++ src/components/Hoff.js
@@ -1,19 +1,23 @@
+import { BsFillPatchCheckFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
 import "./Hoff.css"
-
 function Hoff() {
 return (
     
-<div className=" text-sm hoff bg-gray-200 border-b-4 border-green-400 rounded-lg">
+<div className="p-2 text-sm hoff bg-gray-200 border-b-4 border-green-400 rounded-lg relative">
+    <IconContext.Provider value={{color:'green'}} >
+            <BsFillPatchCheckFill style={{'position':"absolute","top":"-9px","right":"-9px"}}/>
+    </IconContext.Provider>
     <div className="bold text-base">
         <h1>Hoff's Matchups of the Week</h1>
     </div>
-    <ol className="ranking-list text-sm ml-4">
-        <li>Hoff vs. Jacob</li>
-        <li>calvin vs. Phaucette</li>
-        <li>Wes vs. Mad Dawg</li>
-        <li>Colin vs. Hunter</li>
-        <li>Joel vs. Ewic</li>
-        <li>Clog vs. Jay-rod</li>
+    <ol className="ranking-list-hoff text-sm">
+        <li>1. Hoff vs. Jacob</li>
+        <li>2. calvin vs. Phaucette</li>
+        <li>3. Wes vs. Mad Dawg</li>
+        <li>4. Colin vs. Hunter</li>
+        <li>5. Joel vs. Ewic</li>
+        <li>6. Clog vs. Jay-rod</li>
     </ol>
 </div>
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -10,13 +10,16 @@ function LandingPage(){
         <div className="p-4">
           
           <Quote></Quote>
-          <div className="m-4"></div>
-          <WeeklyBreakdown></WeeklyBreakdown>
 
-          <div className="flex flex-row w-full gap-4">
+          <div className="m-4"></div>
+          <span className="p-1 rounded bg-green-200 w-fit font-bold">LTS Rankings</span>
+          <div className="flex flex-row w-full gap-4 mt-4 mb-4">
             <Hoff></Hoff>
             <Phaucette></Phaucette>
           </div>
+          <WeeklyBreakdown></WeeklyBreakdown>
+
+          
           
           <Poll></Poll>
 
