621a2526a8e03fea3bcb8b9be57693544f1da0df
jah <hilliardjoel05@gmail.com>
2023-10-11 17:10:30 -0600

    minor bug fixes

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -53,12 +53,12 @@ function Teams(){
                 </div>
                 <div className="flex flex-row py-1 gap-2 overflow-y-scroll no-scrollbar">
                     {teams && Object.keys(teams).map(team => (
-                        <div style={{"minWidth":"120px"}} key={team} onClick={()=>{setTeam(teams[team]); setIsOpen(!isOpen)}} className="px-4 py-2 text-sm text-green-700 hover:bg-green-400 hover:text-green-900 bg-white border border-green-600 text-center cursor-pointer whitespace-nowrap rounded-full min-w-40 overflow-x-hidden" role="menuitem">
+                        <div style={{"minWidth":"120px"}} key={team} onClick={()=>{setTeam(teams[team]); setIsOpen(!isOpen)}} className="px-4 py-2 text-sm text-green-700 hover:bg-green-400 hover:text-green-900 bg-white border border-green-600 text-center cursor-pointer whitespace-nowrap rounded min-w-40 overflow-x-hidden" role="menuitem">
                             <img
                             alt="pfp"
                             onError={(e) => { e.target.onerror = null; e.target.src="https://www.gravatar.com/avatar/487f7b22f68312d2c1bbc93b1aea445b?s=50&d=identicon&r=PG" }}
-                            style={{'height':'20px', 'width':'20px'}} className="rounded-full" src={teams[team]["logo_url"]}/>
-                            {teams[team]["team_name"]}
+                            style={{'height':'20px', 'width':'20px'}} className="rounded-full mx-auto" src={teams[team]["logo_url"]}/>
+                            <span className="text-center text-xs truncate">{teams[team]["team_name"]}</span>
                         </div>
                     ))}
                     </div>
