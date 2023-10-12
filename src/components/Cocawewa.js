f913e7b847c9736d6b7729e4e7806ea38c08396d
jacob <jacobmiwilliams@gmail.com>
2023-10-12 11:04:31 -0500

    cocawatewa rankings week 6

--- src/components/Cocawewa.js
+++ src/components/Cocawewa.js
@@ -1,56 +1,38 @@
-import "./Phaucette.css"
-import {IconContext} from 'react-icons';
-import {  BsFillPatchCheckFill} from 'react-icons/bs'
+import { BsFillPatchCheckFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
+import "./Hoff.css"
 function Cocawewa() {
-    const rankings = [
-        "Joel",
-        "Hunter",
-        "Colin",
-        "Ethan",
-        "Eric",
-        "Hoff",
-        "Faucette",
-        "Steven",
-        "Jacob",
-        "Calvin",
-        "Wes",
-        "Jayrod",
-    ];
+    const matchup_rankings = [
+        "Hunter vs. Joel",
+        "Hoff vs. Clog",
+        "Faucette vs. Calvin",
+        "Ethan vs. Wes",
+        "Eric vs. Colin",
+        "Jayrod vs. Jacob",
+    ]
+
 return (
     
-    <div className="p-2 text-sm phaucette bg-gray-150 rounded-lg relative">
-        <IconContext.Provider value={{color:'green'}} >
-            <BsFillPatchCheckFill style={{'position':"absolute","top":"-9px","right":"-9px"}}/>
-        </IconContext.Provider>
-        <h2 className="text-base font-bold mb-6 text-gray-800">Ewic's Rankings</h2>
-        <div className="flex flex-row gap-1">
-            <div className="flex space-x-4">
-                <ul className="flex-1 space-y-2">
-                {rankings.slice(0,rankings.length/2).map((person,i) =>{
-                    return(
-                        <li className="p-1 bg-gray-50 border-2 border-gray-200 rounded-md">
-                            <span className="text-sm font-bold text-green-500 whitespace-nowrap"><span className="text-xs">{i+1}.</span> {person}</span>
-                        </li>
-                    )
-                })}
+<div className="p-2 text-sm hoff  rounded-lg relative overflow-visible">
+    <IconContext.Provider value={{color:'green'}} >
+            <BsFillPatchCheckFill style={{'position':"absolute", "top":"-9px", "right":"-9px", 'zIndex': 1000}}/>
+    </IconContext.Provider>
 
-                </ul>
-            </div>
-            <div className="flex space-x-4 ">
-                <ul className="flex-1 space-y-2">
-                {rankings.slice(rankings.length/2  ,rankings.length-1 + 1).map((person,i) =>{
-                    return(
-                        <li className="p-1 bg-gray-50 border-2 border-gray-200 rounded-md">
-                            <span className="text-sm font-bold text-green-500 whitespace-nowrap"><span className="text-xs">{i+rankings.length/2 + 1}.</span> {person}</span>
-                        </li>
-                    )
-                })}
+    <h2 className="text-base font-bold mb-6 text-gray-800">Ewic's Rankings</h2>
+        <div className="flex space-x-4">
+            <ul className="flex-1 space-y-2">
+            {matchup_rankings.map((person,i) =>{
+                return(
+                    <li className="p-1 bg-gray-50 border-2 border-gray-200 rounded-md">
+                        <span className="text-sm font-bold text-green-500 whitespace-nowrap"><span className="text-xs">{i+1}.</span> {person}</span>
+                    </li>
+                )
+            })}
 
-                </ul>
-            </div>
+            </ul>
         </div>
-    </div>
-  
+</div>
+
 );
 }
 
