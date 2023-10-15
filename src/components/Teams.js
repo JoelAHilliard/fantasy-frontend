3ab19fbce7ccfa1c2585d164c58aec798172a534
JoelAHilliard <53853435+JoelAHilliard@users.noreply.github.com>
2023-10-15 17:15:39 +0000

    Update Teams.js

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
+
 import { getTeams } from "../fantasyService";
+
 import TeamBreakdown from "./TeamBreakdown";
 
+import { useParams, useNavigate } from 'react-router-dom';
+
 function Teams(){
-    
+
+    const { teamId } = useParams();
+
+    const navigate = useNavigate();
 
     const [teams,setTeams] = useState(null);
     
@@ -16,14 +23,21 @@ function Teams(){
             getTeams()
             .then(responseData => {
                 setTeams(responseData);
-                setTeam(responseData[0])
+
+                if(teamId){
+                    for(const team in responseData){
+                        if(responseData[team].team_id === Number(teamId)){
+                            setTeam(responseData[team])
+                        }
+                    }
+                }
             })
             .catch(error => {
                 console.error("Failed to get matchups:", error);
             });
         }
         getData();
-    },[]);
+    },[teamId]);
 
    
 
@@ -54,7 +68,7 @@ function Teams(){
                 </div>
                 <div className="ml-3 flex flex-row py-1 gap-2 overflow-y-scroll no-scrollbar">
                     {teams && Object.keys(teams).map(team => (
-                        <div style={{"minWidth":"120px"}} key={team} onClick={()=>{setTeam(teams[team]); setIsOpen(!isOpen)}} className="px-4 py-2 text-sm text-green-700 hover:bg-green-400 hover:text-green-900 bg-white border border-green-600 text-center cursor-pointer whitespace-nowrap rounded min-w-40 overflow-x-hidden" role="menuitem">
+                        <div style={{"minWidth":"120px"}} key={team} onClick={()=>{setTeam(teams[team]); setIsOpen(!isOpen); navigate(`/teams/${teams[team].team_id}`);}} className="px-4 py-2 text-sm text-green-700 hover:bg-green-400 hover:text-green-900 bg-white border border-green-600 text-center cursor-pointer whitespace-nowrap rounded min-w-40 overflow-x-hidden" role="menuitem">
                             <img
                             alt="pfp"
                             onError={(e) => { e.target.onerror = null; e.target.src="https://www.gravatar.com/avatar/487f7b22f68312d2c1bbc93b1aea445b?s=50&d=identicon&r=PG" }}
@@ -65,7 +79,8 @@ function Teams(){
                     </div>
                 </div>
             </div>
-            <TeamBreakdown team={team} teams={teams}></TeamBreakdown>
+            {team?
+            <TeamBreakdown team={team} teams={teams}></TeamBreakdown>:null}
         
         </div>
         :
